c9fe8ff47aaeb711cd72b2af850b29e7519ac87f
belindaHBTN <6372@holbertonstudents.com>
2023-09-24 05:43:48 +0000

    2. Status code

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+const process = require('process');
+
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) console.log(err);
+  console.log(response.statusCode);
+});
