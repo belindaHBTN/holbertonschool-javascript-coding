62159a60b791cd55fdbd4f47e187195fa5c55a18
belindaHBTN <6372@holbertonstudents.com>
2023-10-10 10:50:18 +1100

    added module.exports

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,20 @@
-const readline = require('readline').createInterface({
+const readline = require('readline');
+
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.question('', (name) => {
   console.log(`Your name is: ${name}`);
-  readline.close();
+  rl.close();
 });
 
-readline.on('close', () => {
+rl.on('close', () => {
   console.log('This important software is now closing');
+  process.exit(0);
 });
+
+module.exports = rl;
