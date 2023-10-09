ed1e05f1e3ab816bc618ede112e6f3b19bb27ce0
belindaHBTN <6372@holbertonstudents.com>
2023-10-09 23:08:15 +1100

    added close method to close program automatically

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,18 +3,11 @@ const readline = require('readline').createInterface({
   output: process.stdout,
 });
 
-const main = () => {
-  readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
-    console.log(`Your name is: ${name}`);
-  });
+readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}`);
+  readline.close();
+});
 
-  readline.on('close', () => {
-    console.log('This important software is now closing');
-  });
-};
-
-if (require.main === module) {
-  main();
-}
-
-module.exports = main;
+readline.on('close', () => {
+  console.log('This important software is now closing');
+});
