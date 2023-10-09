78cf2d24c3c93f8684b48929827fe1df708de269
belindaHBTN <6372@holbertonstudents.com>
2023-10-09 22:56:14 +1100

    wrapped code with a function

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,10 +3,18 @@ const readline = require('readline').createInterface({
   output: process.stdout,
 });
 
-readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-});
+const main = () => {
+  readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
+    console.log(`Your name is: ${name}`);
+  });
 
-readline.on('close', () => {
-  console.log('This important software is now closing');
-});
+  readline.on('close', () => {
+    console.log('This important software is now closing');
+  });
+};
+
+if (require.main === module) {
+  main();
+}
+
+module.exports = main;
