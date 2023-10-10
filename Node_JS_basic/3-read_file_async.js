995d0dc39ba0b0777b699b7fce3b35417fcdc51d
belindaHBTN <6372@holbertonstudents.com>
2023-10-10 16:51:00 +1100

    3. Reading a file asynchronously with Node JS

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,27 @@
+const { readFile } = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    const data = await readFile(path, 'utf-8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    const studentData = lines.slice(1).map((line) => line.split(','));
+    console.log(`Number of students: ${studentData.length}`);
+
+    const cs = [];
+    const swe = [];
+    for (let i = 0; i < studentData.length; i += 1) {
+      if (studentData[i][3] === 'CS') {
+        cs.push(studentData[i][0]);
+      } else if (studentData[i][3] === 'SWE') {
+        swe.push(studentData[i][0]);
+      }
+    }
+    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
