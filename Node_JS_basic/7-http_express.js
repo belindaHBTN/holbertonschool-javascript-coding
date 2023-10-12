339b5b76cc25e308b9ab5916538706db2af6e301
belindaHBTN <6372@holbertonstudents.com>
2023-10-12 14:21:04 +1100

    7. Create a more complex HTTP server using Express

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const { readFile } = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    const data = await readFile(path, 'utf-8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    const studentData = lines.slice(1).map((line) => line.split(','));
+
+    const cs = [];
+    const swe = [];
+    for (let i = 0; i < studentData.length; i += 1) {
+      if (studentData[i][3] === 'CS') {
+        cs.push(studentData[i][0]);
+      } else if (studentData[i][3] === 'SWE') {
+        swe.push(studentData[i][0]);
+      }
+    }
+    return (`Number of students: ${studentData.length}\nNumber of students in CS: ${cs.length}. List: ${cs.join(', ')}\nNumber of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const title = 'This is the list of our students\n';
+  const result = await countStudents('database.csv');
+  try {
+    res.send(title + result);
+  } catch (error) {
+    res.send('Internal Server Error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+module.exports = app;
